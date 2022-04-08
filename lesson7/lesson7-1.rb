762f61b83807530737d73e9109a5e88550f0a38c
EC2 Default User <ec2-user@ip-172-31-92-138.ec2.internal>
2022-04-08 09:02:42 +0000

    [finish]2-7

---
+++ lesson7/lesson7-1.rb
@@ -0,0 +1,8 @@
+puts "計算をはじめます"
+puts "2つの値を入力してください"
+first_num=gets.to_i
+second_num=gets.to_i
+total_num=first_num*second_num
+puts "計算結果を出力します"
+puts "a*b=#{total_num}"
+puts "計算を終了します"
---
+++ lesson7/lesson7-2.rb
@@ -0,0 +1,24 @@
+n=1
+a=0
+b=0
+
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+num=gets.to_i
+
+while n<=num do
+  puts "#{n}回目の計算"
+  puts "2つの値を入力してください"
+  a=gets.to_i
+  b=gets.to_i
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します"
+  puts "a+b=#{a+b}"
+  puts "a+b=#{a-b}"
+  puts "a+b=#{a*b}"
+  puts "a+b=#{a/b}"
+  n+=1
+end
+
+puts "計算を終了します"
